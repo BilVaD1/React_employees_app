1f8d34fe293a0cac9d857477480a192f2d68cac8
Vadym <bilvad1@gmail.com>
2022-12-14 17:22:07 +0200

    New CI

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -4,6 +4,8 @@ describe('My employee application', () => {
     it('should add new employee', async () => {
         await HomePage.open()
         await browser.pause(5000)
+        var outerHTML = await $('body').getHTML();
+        console.log(outerHTML)
         await HomePage.addEmployee('Grand', '1300')
         const employees = await $$("ul.app-list.list-group li").length
         expect(employees).toBe(5)
