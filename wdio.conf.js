7761fa14919d8b89deef1b71b702b295c357a820
Vadym <bilvad1@gmail.com>
2022-12-14 10:51:56 +0200

    New fixes

--- wdio.conf.js
+++ wdio.conf.js
@@ -99,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost:3000/React_employees_app',
+    baseUrl: 'https://bilvad1.github.io/React_employees_app/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
