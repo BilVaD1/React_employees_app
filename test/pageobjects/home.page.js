112962e5d6252dea3b5b56de4fd18cab86eb9da1
Vadym <bilvad1@gmail.com>
2022-12-14 10:43:17 +0200

    New fixes

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -8,11 +8,11 @@ const Page = require('./page');
 class HomePage extends Page {
    
     async addEmployee (name, salary){
-        const name = await $('input.form-control.new-post-label:nth-child(1)')
+        const nameField = await $('input.form-control.new-post-label:nth-child(1)')
         const salary = await $('input.form-control.new-post-label:nth-child(2)')
-        await name.waitForExist({ timeout: 10000, timeoutMsg:"The Name field isn't present" })
+        await nameField.waitForExist({ timeout: 10000, timeoutMsg:"The Name field isn't present" })
         await salary.waitForExist({ timeout: 10000, timeoutMsg:"The Salary field isn't present" })
-        await name.setValue(name)
+        await nameField.setValue(name)
         await salary.setValue(salary)
         await $("button[type='submit']").click()
     }
