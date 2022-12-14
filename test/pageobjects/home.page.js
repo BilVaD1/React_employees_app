5ebcb6337dfc3382c4e3bb57bff371d5be62aa8f
Vadym <bilvad1@gmail.com>
2022-12-14 10:04:50 +0200

    New CI

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -8,8 +8,8 @@ const Page = require('./page');
 class HomePage extends Page {
    
     async addEmployee (name, salary){
-        await $('input.form-control.new-post-label:nth-child(1)').setValue(name)
-        await $('input.form-control.new-post-label:nth-child(2)').setValue(salary)
+        await (await $('input.form-control.new-post-label:nth-child(1)')).setValue(name)
+        await (await $('input.form-control.new-post-label:nth-child(2)')).setValue(salary)
         await $("button[type='submit']").click()
     }
 
