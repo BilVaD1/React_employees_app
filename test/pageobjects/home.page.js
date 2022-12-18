5e3d4c8d08374f33ef87342c79502910e5bf9c51
Vadym <bilvad1@gmail.com>
2022-12-18 14:43:41 +0200

    Fixes on test

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -6,10 +6,14 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for a specific page
  */
 class HomePage extends Page {
+
+    async getImputFields (index){
+        return await $(`input.form-control.new-post-label:nth-child(${index})`)
+    }
    
     async addEmployee (name, salary){
-        const nameField = await $('input.form-control.new-post-label:nth-child(1)')
-        const salaryField = await $('input.form-control.new-post-label:nth-child(2)')
+        const nameField = await this.getImputFields(1)
+        const salaryField = await this.getImputFields(2)
         await nameField.waitForExist({ timeout: 10000, timeoutMsg:"The Name field isn't present" })
         await salaryField.waitForExist({ timeout: 10000, timeoutMsg:"The Salary field isn't present" })
         await nameField.setValue(name)
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,10 +3,9 @@ const HomePage = require('../pageobjects/home.page')
 describe('My employee application', () => {
     it('should add new employee', async () => {
         await HomePage.open()
-        await browser.pause(5000)
         await HomePage.addEmployee('Grand', '1300')
         const employees = await $$("ul.app-list.list-group li").length
-        expect(employees).toBe(5)
+        expect(employees).toBe(4)
     })
 })
 
