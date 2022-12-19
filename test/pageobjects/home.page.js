348cf27ecf21369384bfab5ad6f34e6cf42ec8a1
Vadym <bilvad1@gmail.com>
2022-12-19 12:50:09 +0200

    New tests were added

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -10,6 +10,103 @@ class HomePage extends Page {
     async getImputFields (index){
         return await $(`input.form-control.new-post-label:nth-child(${index})`)
     }
+
+    async getDeleteBtn (){
+        return await $('button.btn-trash.btn-sm')
+    }
+
+    async getAllEmployees (){
+        return await $$("ul.app-list.list-group li")
+    }
+
+    async getQtyOfAwards (){
+        return await $('.app-info h2:nth-child(3)').getText()
+    }
+
+    async searchEnter (value) {
+        await $('input.form-control.search-input').setValue(value)
+    } 
+    
+    async selectTab (index){
+        await $(`.btn-group button:nth-child(${index})`).click()
+    }
+
+    async getEmployeesNames (){
+        const employees = await this.getAllEmployees()
+        const names = []
+        for(const employee of employees){
+            const name = await employee.$('span').getText()
+            names.push(name)
+        }
+        return names
+    }
+
+    async getEmployeesSalary (){
+        const employees = await this.getAllEmployees()
+        const salaryList = []
+        for(const employee of employees){
+            const salary = await employee.$('input.list-group-item-input').getAttribute('value')
+            salaryList.push(+salary)
+        }
+        return salaryList
+    }
+
+    async starPromotion (employeeName){
+        const employees = await this.getAllEmployees()
+        for(const args of arguments){
+            for(const employee of employees){
+                const name = await employee.$('span').getText()
+                switch(employeeName == name){
+                    case true:
+                        return await employee.$("//i[@class='fas fa-star']")
+                }
+            }
+        }
+    }
+
+    async getSelectedEmployee (employeeName){
+        const employees = await this.getAllEmployees()
+        for(const employee of employees){
+            const name = await employee.$('span').getText()
+            switch(employeeName == name){
+                case true:
+                    return employee
+           }
+        }
+    }
+
+    async addAward (){
+        const employees = await this.getAllEmployees()
+        first: for(const args of arguments){
+            for(const employee of employees){
+                const name = await employee.$('span').getText()
+                switch(args == name){
+                    case true:
+                        await employee.$('i.fas.fa-cookie').click()
+                        continue first
+                }
+            }
+        }
+    }
+
+    async selectForPromotion (){
+        const employees = await this.getAllEmployees()
+        first: for(const args of arguments){
+            for(const employee of employees){
+                const name = await employee.$('span').getText()
+                switch(args == name){
+                    case true:
+                        await employee.$('span').click()
+                        continue first
+                }
+            }
+        }
+    }
+
+    async deleteEmployee (employeeName){
+        const employee = await this.getSelectedEmployee(employeeName)
+        await employee.$(await this.getDeleteBtn()).click()
+    }
    
     async addEmployee (name, salary){
         const nameField = await this.getImputFields(1)
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,11 +1,51 @@
 const HomePage = require('../pageobjects/home.page')
 
 describe('My employee application', () => {
-    it('should add new employee', async () => {
+    
+    beforeEach(async function(){
         await HomePage.open()
+      })
+
+
+    it('should add new employee', async () => {
         await HomePage.addEmployee('Grand', '1300')
-        const employees = await $$("ul.app-list.list-group li").length
+        const employees = await (await HomePage.getAllEmployees()).length
         expect(employees).toBe(4)
+    })
+
+    it('should delete employee', async () => {
+        await HomePage.deleteEmployee("John C.")
+        const names = await HomePage.getEmployeesNames()
+        expect(names).not.toContain('John C.')
+    })
+
+    it('should add an award to the employee', async () => {
+        await HomePage.addAward('John C.', 'Carl W.')
+        const awards = await HomePage.getQtyOfAwards()
+        expect(awards).toContain('2')
+    })
+
+    it('should select employee(s) for promotion', async () => {
+        await HomePage.selectForPromotion('John C.', 'Carl W.')
+        await HomePage.selectTab(2)
+        const employees = await (await HomePage.getAllEmployees()).length
+        expect(employees).toBe(2)
+    })
+
+    it('should search employee(s)', async () => {
+        await HomePage.searchEnter('lex')
+        const names = await HomePage.getEmployeesNames()
+        for(const name of names){
+            expect(name).toMatch('lex')
+        }
+    })
+
+    it('should show the employee(s) with salary over 1000$', async () => {
+        await HomePage.selectTab(3)
+        const salary = await HomePage.getEmployeesSalary()
+        for(const el of salary){
+            expect(el).toBeGreaterThanOrEqual(1000)
+        }
     })
 })
 
